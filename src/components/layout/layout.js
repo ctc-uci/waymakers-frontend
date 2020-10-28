5b0f43b49d3f975a4ef8da76cee35611243ce91e
Leon <leonlin.48@gmail.com>
2020-10-27 23:55:32 -0700

    add and trim boilerplate

---
+++ src/components/layout/layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../header/header';
+import Footer from '../footer/footer';
+
+import './layout.css';
+
+const Layout = () => (
+  <div className="layout">
+    <Header />
+    <Footer />
+  </div>
+);
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// import Header from './components/header/header';
+// import Footer from './components/footer/footer';
+import Layout from './components/layout/layout';
+// import App from './App';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <Header />
+    <Footer /> */}
+    <Layout />
   </React.StrictMode>,
   /* eslint-disable no-undef */
   document.getElementById('root'),
