4976730d1a83b7b0c59c50ea81bd04e607d60c31
Leon <leonlin.48@gmail.com>
2020-10-28 00:09:38 -0700

    remove comments

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import Header from './components/header/header';
-// import Footer from './components/footer/footer';
 import Layout from './components/layout/layout';
-// import App from './App';
+
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <Header />
-    <Footer /> */}
     <Layout />
   </React.StrictMode>,
   /* eslint-disable no-undef */
