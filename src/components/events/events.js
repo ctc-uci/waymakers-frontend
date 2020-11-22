3731fbedf9fd13af5d9bbf8cd4283dae372f5ee9
Rostam Vakhshoori <rvakhsho@uci.edu>
2020-11-21 16:12:58 -0800

    Added axios API

--- src/components/events/events.js
+++ src/components/events/events.js
@@ -2,6 +2,8 @@ import React from 'react';
 // import EventList from './event-list/eventList';
 import EventsView from './events-view/eventsView';
 
+const axios = require('axios');
+
 /*
 {
   name:
@@ -18,17 +20,37 @@ const Events = () => {
 
   // GET events from the backend
   // filter by MyEvents and CurrentEvents to pass into EventsView
-  const currentEvents = [
-    {
-      title: 'currentEvent 1',
-      start: '2020-11-18T11:00:00',
-      end: '2020-11-18T13:00:00',
-      description: 'This is a test woohoo',
-    },
-    {
-      title: 'current event 2', date: '2020-11-19',
-    },
-  ];
+  // const currentEvents = [
+  //   {
+  //     title: 'currentEvent 1',
+  //     start: '2020-11-18T11:00:00',
+  //     end: '2020-11-18T13:00:00',
+  //     description: 'This is a test woohoo',
+  //   },
+  //   {
+  //     title: 'current event 2', date: '2020-11-19',
+  //   },
+  // ];
+  let currentEvents = {};
+  axios.get('http://localhost:2999/events/').then((resp) => {
+    // eslint-disable-next-line
+    console.log(resp.data.toString());
+    currentEvents = JSON.parse(resp.data.toString());
+  });
+  // eslint-disable-next-line
+  console.log(currentEvents);
+  // axios.get(':3001/events/')
+  //   .then(function (response) {
+  //     // handle success
+  //     const currentEvents = JSON.parse(response);
+  //   })
+  //   .catch(function (error) {
+  //     // handle error
+  //     console.log(error);
+  //   })
+  //   .then(function () {
+  //     // always executed
+  //   });
 
   const myEvents = [
     {
