49b0017c2283267fe78709a16501bd38494e4983
Zubair Sidhu <sidhuzubair@gmail.com>
2020-11-30 17:52:36 -0600

    Added Table component to inventory.js

--- src/components/inventory/inventory.js
+++ src/components/inventory/inventory.js
@@ -3,15 +3,53 @@ import React, { useEffect, useState } from 'react';
 import './inventory.css';
 // import DisplayItem from './DisplayItem';
 import CategoryMenu from './CategoryMenu';
+import AddItem from './AddItem';
+import Table from './Table';
 
 const Inventory = () => {
+  // Current list of items to display
+  const [items, setItems] = useState([]);
   // Category selected by user in CategoryMenu
   const [selectedCategory, setSelectedCategory] = useState('');
   // Current list of categories (in scroll menu)
   const [categoryList, setCategoryList] = useState([]);
 
+  const searchSubstring = '';
+
+  // Request items from server
+  const getItems = async () => {
+    console.log('Getting items');
+    try {
+      // NOT SEARCHING
+      if (searchSubstring === '') {
+        const response = await fetch(
+          `http://localhost:3000/inventory/${selectedCategory}`,
+        );
+        const jsonData = await response.json();
+        setItems(jsonData);
+      // SEARCH for item with ONLY SUBSTRING
+      } else if (selectedCategory === '') {
+        const response = await fetch(
+          `http://localhost:3000/inventory/search/${searchSubstring}/`,
+        );
+        const jsonData = await response.json();
+        setItems(jsonData);
+      // SEARCH for item with SUBSTRING and CATEGORY
+      } else {
+        const response = await fetch(
+          `http://localhost:3000/inventory/search/${searchSubstring}/${selectedCategory}`,
+        );
+        const jsonData = await response.json();
+        setItems(jsonData);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   // Request categories from server
   const getCategories = async () => {
+    console.log('Getting categories');
     try {
       const response = await fetch('http://localhost:3000/category/');
       const jsonData = await response.json();
@@ -25,18 +63,24 @@ const Inventory = () => {
   // Once component mounts, call getCategories
   useEffect(() => {
     getCategories();
-    console.log('in useEffect');
   }, []);
+
+  // Updates items list when selectedCategory changes or searchSubstring changes
+  useEffect(() => {
+    getItems();
+  }, [selectedCategory]);
 
   return (
     <div className="inventory">
       <h1>Inventory</h1>
       <h1 className="text-center mt-5">Warehouse #1</h1>
+      <AddItem />
       <CategoryMenu
         selectedCategory={selectedCategory}
         categoryList={categoryList}
         setSelectedCategory={setSelectedCategory}
       />
+      <Table items={items} getItems={getItems} />
       {/* <AddItem />
       <ScrollMenu
         data={menu}
