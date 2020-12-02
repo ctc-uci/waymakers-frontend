cc130771d5db2bd3c8f606637f395ad0b3ec8dc0
Megha Kak <meghakak1@gmail.com>
2020-12-02 21:08:09 +0000

    Resolve "User Login"

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  Card, Button, Alert,
+} from 'react-bootstrap';
+import auth from '../firebase/firebase';
 
-const Dashboard = () => (
-  <div className="dashboard">
-    <h1>Dashboard</h1>
-  </div>
-);
+const Dashboard = () => {
+  const history = useHistory();
+  const [error, setError] = useState('');
+
+  async function logout() {
+    try {
+      await auth.signOut();
+      history.push('/login');
+      // Sign-out successful
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
+  return (
+    <div className="d-flex align-items-center justify-content-center">
+      <div className="w-100 login-container">
+        <Card className="w-100">
+          <Card.Body>
+            <h2 className="text-center mb-4">Dashboard</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Button onClick={logout} to="/login" className="w-100" type="submit" variant="primary">Log Out</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
