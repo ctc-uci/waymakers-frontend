aec60840e45306eabe34e9265e0414750aef5b61
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2021-01-05 17:43:22 -0800

    handling put requests before delete

--- src/components/inventory/redux/actions.js
+++ src/components/inventory/redux/actions.js
@@ -107,16 +107,9 @@ export const startEdits = () => ({
 export const saveEdits = () => async (dispatch) => {
   console.log('in saveEdits');
   const editPromises = [];
+  const deletePromises = [];
 
-  // Populating list with DELETE requests for each deleted item
-  const deletedItems = [...store.getState().edits.deletedItems];
-  deletedItems.forEach(async (id) => {
-    editPromises.push(
-      axios.delete(`http://localhost:3000/inventory/${id}`),
-    );
-  });
-
-  // Populating list with PUT requests for each edited item
+  // Populating edited list with PUT requests for each edited item
   const editedItems = { ...store.getState().edits.editedItems };
   Object.keys(editedItems).forEach(async (id) => {
     editPromises.push(
@@ -124,16 +117,44 @@ export const saveEdits = () => async (dispatch) => {
     );
   });
 
+  // Populating list with DELETE requests for each deleted item
+  const deletedItems = [...store.getState().edits.deletedItems];
+  deletedItems.forEach(async (id) => {
+    deletePromises.push(
+      axios.delete(`http://localhost:3000/inventory/${id}`),
+    );
+  });
+
   // Populating list with DELETE requests for each deleted category
   const deletedCategories = [...store.getState().edits.deletedCategories];
   deletedCategories.forEach(async (id) => {
-    editPromises.push(
+    deletePromises.push(
       axios.delete(`http://localhost:3000/category/${id}`),
     );
   });
 
-  // Perform all requests concurrently
+  // Perform all put requests concurrently
   Promise.all(editPromises)
+    .catch((error) => { console.error(error); })
+    .then(() => {
+      // Dispatch editsSaved action
+      // dispatch({ type: 'edits/editsSaved', payload: {} });
+      // Fetch items list again
+      // dispatch(fetchItems());
+      // Fetch category list again
+      // dispatch(fetchCategories());
+      if (deletePromises.length === 0) {
+        // Dispatch editsSaved action
+        dispatch({ type: 'edits/editsSaved', payload: {} });
+        // Fetch items list again
+        dispatch(fetchItems());
+        // Fetch category list again
+        dispatch(fetchCategories());
+      }
+    });
+
+  // Perform all delete requests concurrently
+  Promise.all(deletedItems)
     .catch((error) => { console.error(error); })
     .then(() => {
       // Dispatch editsSaved action
