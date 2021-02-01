0c07779f7d2c766f1e40d9ee4267cdb57816c785
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2021-01-31 18:28:26 -0800

    changed all volunteers table to better match mid fidel

--- src/components/admin/volunteer-event-aggregate-page/event-data-page/all-volunteers/allVolunteers.js
+++ src/components/admin/volunteer-event-aggregate-page/event-data-page/all-volunteers/allVolunteers.js
@@ -15,27 +15,30 @@ const AllVolunteers = (prop) => {
 
   const [allVolunteers, setAllVolunteers] = useState([]);
 
-  const [selectedTier, setTier] = useState('');
+  const [sortingMethod, setSortingMethod] = useState('0');
+
+  const [totalHours, setTotalHours] = useState(0);
 
   const paramQuery = {
     params: {
       event: prop.event.id,
-      tier: selectedTier,
+      sortingMethod,
     },
   };
 
   const getAllVolunteers = async () => {
     const volunteers = await instance.get('volunteerData/all/', paramQuery);
     setAllVolunteers(volunteers.data);
+    setTotalHours(volunteers.data.map((v) => v.sum).reduce((acc, v) => acc + v));
   };
 
   useEffect(() => {
     getAllVolunteers();
-  }, [selectedTier]);
+  }, [sortingMethod]);
 
   const VolunteerItem = (volunteer, index) => (
     <>
-      <tr data-toggle="collapse" data-target={`.order${index}`}>
+      <tr key={volunteer.userid} data-toggle="collapse" data-target={`.order${index}`}>
         <th className="border-dark">
           <div className="volunteer-info">
             <div className="volunteer-name">
@@ -44,7 +47,7 @@ const AllVolunteers = (prop) => {
               {volunteer.lastname}
             </div>
             <div className="volunteer-position">
-              {volunteer.tier}
+              {volunteer.sum}
             </div>
           </div>
         </th>
@@ -52,11 +55,17 @@ const AllVolunteers = (prop) => {
       <tr>
         <td className="collapsed-info" style={{ padding: 0 }}>
           <div className={`collapse order${index}`}>
-            Number of hours:
+            Position:
             {' '}
-            {volunteer.sum}
+            {volunteer.permissions}
             <br />
-            Other Information:
+            Volunteer Tier:
+            {' '}
+            {volunteer.tier}
+            <br />
+            Age:
+            {' '}
+            {volunteer.date_part}
           </div>
         </td>
       </tr>
@@ -65,44 +74,64 @@ const AllVolunteers = (prop) => {
 
   // TODO: All volunteers view box
   return (
-    <table className="table table-bordered volunteer-table border-dark">
-      <thead className="border-dark">
-        <th scope="col" className="all-volunteers-table-head">
-          <h1>List of Volunteers</h1>
-          <select
-            className="form-control all-volunteers-tier-selection"
-            id="tiers"
-            name="tiers"
-            onChange={(e) => setTier(e.target.value)}
-          >
-            <option value="" selected>All Tiers</option>
-            {/* Dropdown menu for tiers */}
-            <option value="1">Tier 1</option>
-            <option value="2">Tier 2</option>
-            <option value="3">Tier 3</option>
-            <option value="4">Tier 4</option>
-          </select>
-        </th>
-      </thead>
-      <tbody>
-        {allVolunteers.map((volunteer, index) => (
-          VolunteerItem(volunteer, index)
-        ))}
-      </tbody>
-      <div className="volunteer-cardinality">
-        <div className="volunteer-number-label">
-          <h2>
-            Total:
-            {' '}
-          </h2>
-        </div>
-        <div className="volunteer-number">
-          <h2>
-            {allVolunteers.length}
-          </h2>
-        </div>
+    <div className="all-volunteers-table">
+      <h1>List of Volunteers</h1>
+      <div className="all-volunteers-table-header">
+        <h3>Sort by</h3>
+        <select
+          className="form-control all-volunteers-tier-selection"
+          id="tiers"
+          name="tiers"
+          onChange={(e) => setSortingMethod(e.target.value)}
+        >
+          <option value="0" selected>None</option>
+          <option value="1">A-Z</option>
+          <option value="2">Z-A</option>
+          <option value="3">Tier</option>
+        </select>
       </div>
-    </table>
+      <table className="table table-bordered volunteer-table border-dark">
+        <thead className="border-dark">
+          <th scope="col" className="all-volunteers-table-head">
+            <h1>Name</h1>
+            {/* <select
+              className="form-control all-volunteers-tier-selection"
+              id="tiers"
+              name="tiers"
+              onChange={(e) => setTier(e.target.value)}
+            >
+              <option value="" selected>All Tiers</option>
+              <option value="1">Tier 1</option>
+              <option value="2">Tier 2</option>
+              <option value="3">Tier 3</option>
+              <option value="4">Tier 4</option>
+            </select> */}
+            <h1>Number of hours</h1>
+          </th>
+        </thead>
+        <tbody>
+          {allVolunteers.map((volunteer, index) => (
+            VolunteerItem(volunteer, index)
+          ))}
+        </tbody>
+        <div className="volunteer-cardinality">
+          <div className="volunteer-number">
+            <h2>
+              Total People:
+              {' '}
+              {allVolunteers.length}
+            </h2>
+          </div>
+          <div className="volunteer-total-hours">
+            <h2>
+              Total Hours:
+              {' '}
+              {totalHours}
+            </h2>
+          </div>
+        </div>
+      </table>
+    </div>
   );
 };
 
