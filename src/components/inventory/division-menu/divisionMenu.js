dd4770ef11ab8c0c835f8caa6d648d242243ff54
Jane Vo <vo.janehanh@gmail.com>
2021-02-20 21:49:24 -0800

    Styled division menu

--- src/components/inventory/division-menu/divisionMenu.js
+++ src/components/inventory/division-menu/divisionMenu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import WarehouseMenu from './warehouseMenu';
 
@@ -8,27 +8,67 @@ import { getDivisions, getSelectedDivisionID, getWarehouses } from '../redux/sel
 
 import './divisionMenu.css';
 
-const DivisionMenu = (prop) => (
-  <div className="menu-container">
-    <div className="division-menu-container">
-      <select
+const DivisionMenu = (prop) => {
+  const [currentDivision, setCurrentDiviosn] = useState('All Divisions');
+  const [open, setOpen] = useState(false);
+
+  const handleArrowClick = () => {
+    if (open) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
+
+  const handleDivisionClick = (e, divName) => {
+    store.dispatch(changeSelectedDivision(parseInt(e.target.value, 10)));
+    setCurrentDiviosn(divName);
+  };
+
+  const menu = (list) => (
+    <div>
+      <div
         name="category"
-        className="division-menu"
+        className="division-menu--list"
         value={prop.selectedDivision}
-        onChange={(e) => { store.dispatch(changeSelectedDivision(parseInt(e.target.value, 10))); }}
       >
         {/* Creating dropdown menu items from divisions list */}
         {/* division.div_name is displayed, but the value of the option will be the ID */}
-        {Object.entries(prop.divisionList)
+        {Object.entries(list)
           .sort((a, b) => (a.id > b.id ? 1 : -1))
           .map(([id, division]) => (
-            <option key={id} value={id}>{division.div_name}</option>
+            <button
+              type="button"
+              key={id}
+              value={id}
+              className="division-menu--list-item"
+              onClick={(e) => handleDivisionClick(e, division.div_name)}
+            >
+              {division.div_name}
+            </button>
           ))}
-      </select>
+      </div>
     </div>
-    <WarehouseMenu warehouseList={prop.warehouseList} />
-  </div>
-);
+  );
+
+  return (
+    <div className="menu-container">
+      <div className="division-menu-container">
+        <div className="division-menu--top">
+          {currentDivision}
+          <button
+            type="button"
+            aria-label="arrow"
+            onClick={handleArrowClick}
+            className={open ? 'division-menu--close' : 'division-menu--open'}
+          />
+        </div>
+        {open && menu(prop.divisionList)}
+      </div>
+      <WarehouseMenu warehouseList={prop.warehouseList} />
+    </div>
+  );
+};
 
 // Connecting component props to redux state
 const mapStateToProps = (state) => ({
