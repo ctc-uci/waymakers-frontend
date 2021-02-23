0b0685f77d3fe49468cd0c823f7d0ceda93470ea
Zubair Sidhu <sidhuzubair@gmail.com>
2021-02-22 21:41:37 -0600

    Add basic backend connection to qualification lists page

--- src/components/admin/qualification-lists/qualificationLists.js
+++ src/components/admin/qualification-lists/qualificationLists.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './qualificationLists.css';
 
-const QualificationLists = () => (
-  <div>
-    <h1>QualificationLists</h1>
-  </div>
-);
+const axios = require('axios');
+
+const QualificationLists = () => {
+  const [qualificationLists, setQualificationLists] = useState([]);
+
+  // Fetching qualificationLists from the server
+  const getQualificationLists = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}/qualifications`,
+        { withCredentials: true },
+      );
+      setQualificationLists(response.data);
+    } catch (err) {
+      // eslint-disable-next-line
+      console.error(err);
+    }
+  };
+
+  // Get qualificationsLists on page load
+  useEffect(() => {
+    getQualificationLists();
+  }, []);
+
+  return (
+    <div>
+      <h1>QualificationLists</h1>
+      <pre>{JSON.stringify(qualificationLists, null, 2) }</pre>
+    </div>
+  );
+};
 
 export default QualificationLists;
