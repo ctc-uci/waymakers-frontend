16071f36a3b332b5a26d6a191404a66d0500a80e
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2021-03-11 22:49:10 -0800

    almost (?) done with pie charts

--- src/components/admin/volunteer-event-aggregate-page/event-data-page/demographics/demographics.js
+++ src/components/admin/volunteer-event-aggregate-page/event-data-page/demographics/demographics.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 import PropTypes from 'prop-types';
-import GenericPieChart from './pie-chart/PieChart';
+import PieChart from './pie-chart/PieChart';
+
+import leftArrow from '../../../../../images/pieChartLeftArrow.svg';
+import rightArrow from '../../../../../images/pieChartRightArrow.svg';
+import './demographics.css';
 
 const axios = require('axios');
 
@@ -10,18 +15,37 @@ const Demographics = ({ event }) => {
     withCredentials: true,
   });
 
+  // Get information for all volunteers
   const [allVolunteers, setAllVolunteers] = useState([]);
+  // Determines which 3 pie charts we display
+  const [startDisplayIndex, setStartDisplayIndex] = useState(0);
+  // List of pie chart attributes/labels which helps us map pie charts
+  // Attribute is the name of column stored in the actual database
+  // Label is the name of the pie chart
+  const pieChartLabels = [
+    { attribute: 'gender', label: 'gender' },
+    { attribute: 'age', label: 'age group' },
+    { attribute: 'tier', label: 'volunteer tier' },
+    { attribute: 'sum', label: 'number of hours' },
+  ];
 
   const getDemographicData = (attribute) => {
     const counter = {};
     allVolunteers.forEach((volunteer) => {
-      console.log(attribute, volunteer[attribute]);
+      /* TODO
+        Set up age groups
+      */
       if (!counter[volunteer[attribute]]) {
         counter[volunteer[attribute]] = 0;
       }
       counter[volunteer[attribute]] += 1;
     });
-    return Object.entries(counter).map(([k, v]) => ({ name: k, value: v }));
+    return Object.entries(counter).map(([k, v]) => ({
+      id: `${k} (${v})`,
+      label: k,
+      value: v,
+      color: 'red',
+    }));
   };
 
   const paramQuery = {
@@ -30,20 +54,52 @@ const Demographics = ({ event }) => {
     },
   };
 
+  // Get all volunteers
   const getAllVolunteers = async () => {
     const volunteers = await instance.get('volunteerData/all/', paramQuery);
-    setAllVolunteers(volunteers.data);
+    const volunteersWithAge = volunteers.data.map((volunteer) => ({
+      ...volunteer,
+      age: moment(new Date()).diff(volunteer.birthdate, 'years'), // Get volunteer age from birthdate
+    }));
+    setAllVolunteers(volunteersWithAge);
   };
 
+  // Get all volunteers on page load
   useEffect(() => {
     getAllVolunteers();
   }, []);
 
+  const handleLeftArrowClick = () => {
+    if (startDisplayIndex > 0) {
+      setStartDisplayIndex(startDisplayIndex - 1);
+    }
+  };
+
+  const handleRightArrowClick = () => {
+    if (startDisplayIndex < pieChartLabels.length - 3) {
+      setStartDisplayIndex(startDisplayIndex + 1);
+    }
+  };
+
   return (
-    <>
-      <GenericPieChart demoInfo={getDemographicData('gender')} label="gender" />
-      <GenericPieChart demoInfo={getDemographicData('tier')} label="tier" />
-    </>
+    <div className="demographics">
+      <h2 className="demographics-title">Demographics</h2>
+      <div className="pie-charts">
+        <button type="button" onClick={handleLeftArrowClick} className={startDisplayIndex > 0 ? 'left-arrow' : 'left-arrow-nonfunctional'}>
+          <img src={leftArrow} alt="left-arrow" />
+        </button>
+        {pieChartLabels.slice(startDisplayIndex, startDisplayIndex + 3)
+          .map(({ attribute, label }) => (
+            <PieChart
+              demoInfo={getDemographicData(attribute)}
+              label={label}
+            />
+          ))}
+        <button type="button" onClick={handleRightArrowClick} className={startDisplayIndex < pieChartLabels.length - 3 ? 'right-arrow' : 'right-arrow-nonfunctional'}>
+          <img src={rightArrow} alt="right-arrow" />
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/components/admin/volunteer-event-aggregate-page/event-data-page/demographics/pie-chart/PieChart.js
+++ src/components/admin/volunteer-event-aggregate-page/event-data-page/demographics/pie-chart/PieChart.js
@@ -1,69 +1,105 @@
 import React from 'react';
-import { PieChart, Pie } from 'recharts';
+// import { PieChart, Pie, ResponsiveContainer } from 'recharts';
+import { ResponsivePie } from '@nivo/pie';
 import PropTypes from 'prop-types';
 
 import '../../../../../../common/vars.css';
 import './PieChart.css';
 
-const colors = ['red', '#8884d8', '#82ca9d'];
+// TODO: Implement different color schemes for pie charts
+// const colors = ['red', '#8884d8', '#82ca9d'];
 
-const GenericPieChart = ({ demoInfo, label }) => {
-  console.log(demoInfo);
-  return (
-    <div className="pie-chart-and-label">
-      <PieChart width={400} height={400} className="chart">
-        <Pie
-          dataKey="value"
-          isAnimationActive={false}
-          data={demoInfo.map((d, index) => (
-            {
-              ...d,
-              fill: colors[index],
-            }
-          ))}
-          cx="50%"
-          cy="50%"
-          outerRadius={80}
-          fill="#8884d8"
-          label={({
-            cx,
-            cy,
-            midAngle,
-            innerRadius,
-            outerRadius,
-            value,
-            index,
-          }) => {
-            const RADIAN = Math.PI / 180;
-            // eslint-disable-next-line
-            const radius = 25 + innerRadius + (outerRadius - innerRadius);
-            // eslint-disable-next-line
-            const x = cx + radius * Math.cos(-midAngle * RADIAN);
-            // eslint-disable-next-line
-            const y = cy + radius * Math.sin(-midAngle * RADIAN);
-            return (
-              <text
-                x={x}
-                y={y}
-                fill="var(--text-color-blue-gray)"
-                textAnchor={x > cx ? 'start' : 'end'}
-                dominantBaseline="central"
-              >
-                {demoInfo[index].name}
-                (
-                {value}
-                )
-              </text>
-            );
-          }}
-        />
-      </PieChart>
-      <p className="chart-label">{label}</p>
-    </div>
-  );
-};
+const PieChart = ({ demoInfo, label }) => (
+  // <div className="pie-chart-and-label">
+  //   <ResponsiveContainer width="100%" height="100%">
+  //     <PieChart className="chart">
+  //       <Pie
+  //         dataKey="value"
+  //         isAnimationActive={false}
+  //         data={demoInfo.map((d, index) => (
+  //           {
+  //             ...d,
+  //             fill: colors[index],
+  //           }
+  //         ))}
+  //         cx="50%" // Position of the pie chart
+  //         cy="50%" // Position of the pie chart
+  //         outerRadius={80}
+  //         label={({
+  //           cx,
+  //           cy,
+  //           midAngle,
+  //           innerRadius,
+  //           outerRadius,
+  //           value,
+  //           index,
+  //         }) => {
+  //           const RADIAN = Math.PI / 180;
+  //           // eslint-disable-next-line
+  //           const radius = 20 + innerRadius + (outerRadius - innerRadius);
+  //           // eslint-disable-next-line
+  //           const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  //           // eslint-disable-next-line
+  //           const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  //           return (
+  //             <text
+  //               className="chart-section-label"
+  //               x={x} // Position of the label
+  //               y={y} // Position of the label
+  //               fontSize="15px"
+  //               fill="var(--text-color-blue-gray)"
+  //               textAnchor={x > cx ? 'start' : 'end'}
+  //               dominantBaseline="middle"
+  //             >
+  //               {demoInfo[index].name}
+  //               (
+  //               {value}
+  //               )
+  //             </text>
+  //           );
+  //         }}
+  //       />
+  //     </PieChart>
+  //   </ResponsiveContainer>
+  //   <p className="chart-label">{label}</p>
+  // </div>
 
-GenericPieChart.propTypes = {
+  // NOTE: There are 2 solutions to labels overlapping (might be more if you look hard enough):
+  // 1. Increase the left/right margins
+  // 2. Decrease the font size
+  <div className="pie-chart-and-label">
+    <ResponsivePie
+      data={demoInfo}
+      // TODO: Make size responsive?
+      // The hard part about this is making sure the labels don't mess up afterwards
+      width={350}
+      height={350}
+      margin={{
+        top: 0,
+        right: 80,
+        bottom: 0,
+        left: 80,
+      }}
+      innerRadius={0}
+      padAngle={0.7}
+      cornerRadius={0}
+      colors={{ scheme: 'nivo' }}
+      borderWidth={1}
+      borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+      radialLabelsSkipAngle={10}
+      radialLabelsTextColor="var(--text-color-blue-gray)"
+      radialLabelsLinkColor="#000"
+      sliceLabelsSkipAngle={10}
+      sliceLabelsTextColor={{ from: 'color' }}
+      theme={{
+        fontSize: 12,
+      }}
+    />
+    <p className="chart-label">{label}</p>
+  </div>
+);
+
+PieChart.propTypes = {
   demoInfo: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired,
     value: PropTypes.number.isRequired,
@@ -71,4 +107,4 @@ GenericPieChart.propTypes = {
   label: PropTypes.string.isRequired,
 };
 
-export default GenericPieChart;
+export default PieChart;
