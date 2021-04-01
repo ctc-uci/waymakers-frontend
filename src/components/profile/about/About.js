5696f38052717dd2c2f9b6333024bcf2ec05e691
meghakak <meghakak1@gmail.com>
2021-04-01 16:44:02 -0700

    Add mobile view styling and switch to svg.

--- src/components/profile/about/About.js
+++ src/components/profile/about/About.js
@@ -2,25 +2,25 @@
 import React from 'react';
 import './About.css';
 
-import cake from '../../../assets/bday.png';
-import people from '../../../assets/ppl.png';
-import building from '../../../assets/building.png';
+import cake from '../../../assets/birthday.svg';
+import people from '../../../assets/volunteer-tier.svg';
+import building from '../../../assets/student.svg';
 import TitledCard from '../../../common/Card/TitledCard';
 
 function About({ birthday, tier, status }) {
   return (
     <TitledCard title="About" className="about-card">
       <p>
-        <img src={cake} alt="" width="15px" height="15px" />
-        {` ${birthday}`}
+        <img className="about-icons" src={cake} alt="" />
+        {`${birthday}`}
       </p>
       <p>
-        <img src={people} alt="" width="15px" height="15px" />
-        {` ${tier}`}
+        <img className="about-icons" src={people} alt="" width="15px" height="15px" />
+        {`${tier}`}
       </p>
       <p>
-        <img src={building} alt="" width="15px" height="15px" />
-        {` ${status}`}
+        <img className="about-icons" src={building} alt="" width="15px" height="15px" />
+        {`${status}`}
       </p>
     </TitledCard>
   );
--- src/components/profile/editAbout/editAbout.js
+++ src/components/profile/editAbout/editAbout.js
@@ -1,28 +1,23 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
+import Datetime from 'react-datetime';
 // import { Link } from 'react-router-dom';
 import './editAbout.css';
+import TitledCard from '../../../common/Card/TitledCard';
+
+import cake from '../../../assets/birthday.svg';
+import people from '../../../assets/volunteer-tier.svg';
+import building from '../../../assets/student.svg';
 
 const EditAbout = ({
   tier, setTier, status, setStatus,
 }) => {
-  const [birthday, setBday] = useState('');
+  const [birthday, setBday] = useState(new Date());
 
   return (
-    <div className="aboutCard">
-      <h2>About</h2>
+    <TitledCard title="About" className="about-card">
       <form>
-        <label htmlFor="bday">Birthday: </label>
-        <input type="date" value={birthday} name="bday" onChange={(e) => setBday(e.target.value)} />
-        <br />
-        <br />
-        <label htmlFor="tier">Volunteer Tier: </label>
-        <input type="text" value={tier} name="tier" onChange={(e) => setTier(e.target.value)} />
-        <br />
-        <br />
-        <label htmlFor="status">Status: </label>
-        <input type="text" value={status} name="status" onChange={(e) => setStatus(e.target.value)} />
         {/* <input type="submit" /> */}
         {/* <Link
           classname="to-view"
@@ -33,9 +28,24 @@ const EditAbout = ({
         >
           Save
         </Link> */}
-      </form>
-    </div>
 
+        <div className="about-input">
+          <img className="about-icons" src={cake} alt="" />
+          <Datetime
+            initialValue={birthday}
+            onChange={(e) => setBday(new Date(e))}
+          />
+        </div>
+        <div className="about-input">
+          <img className="about-icons" src={people} alt="" />
+          <input type="text" value={tier} name="tier" onChange={(e) => setTier(e.target.value)} />
+        </div>
+        <div className="about-input">
+          <img className="about-icons" src={building} alt="" />
+          <input type="text" value={status} name="status" onChange={(e) => setStatus(e.target.value)} />
+        </div>
+      </form>
+    </TitledCard>
   );
 };
 
