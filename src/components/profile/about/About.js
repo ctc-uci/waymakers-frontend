15a325b70beb7b1b36951a918ab0916571b2a3e1
meghakak <meghakak1@gmail.com>
2021-04-01 15:43:09 -0700

    Switch to use of Card component.

--- src/components/profile/about/About.js
+++ src/components/profile/about/About.js
@@ -5,27 +5,24 @@ import './About.css';
 import cake from '../../../assets/bday.png';
 import people from '../../../assets/ppl.png';
 import building from '../../../assets/building.png';
+import TitledCard from '../../../common/Card/TitledCard';
 
 function About({ birthday, tier, status }) {
   return (
-    <div className="aboutCard">
-      <h2>About</h2>
-      <div>
-        <p>
-          <img src={cake} alt="" width="15px" height="15px" />
-          {` ${birthday}`}
-        </p>
-        <p>
-          <img src={people} alt="" width="15px" height="15px" />
-          {` ${tier}`}
-        </p>
-        <p>
-          <img src={building} alt="" width="15px" height="15px" />
-          {` ${status}`}
-        </p>
-      </div>
-    </div>
-
+    <TitledCard title="About" className="about-card">
+      <p>
+        <img src={cake} alt="" width="15px" height="15px" />
+        {` ${birthday}`}
+      </p>
+      <p>
+        <img src={people} alt="" width="15px" height="15px" />
+        {` ${tier}`}
+      </p>
+      <p>
+        <img src={building} alt="" width="15px" height="15px" />
+        {` ${status}`}
+      </p>
+    </TitledCard>
   );
 }
 
