84b3594126ff8d2b5f85ac51fd2fbf067618b99f
Alan Chang <alan@alanchang.xyz>
2021-04-03 02:47:15 -0700

    Start on desktop navbar with links and corresponding CSS

--- src/components/header/desktop/desktopNavbar.js
+++ src/components/header/desktop/desktopNavbar.js
@@ -3,12 +3,21 @@ import { Link } from 'react-router-dom';
 
 import './desktopNavbar.css';
 
-const DesktopNavbar = () => (
-  <div className="navbar">
-    <Link to="/">
-      Temp Link
-    </Link>
-  </div>
-);
+const DesktopNavbar = () => {
+  // dummy isAdmin & isVolunteer vars; change with addition of logic to get user perms
+  const isAdmin = true;
+  const isVolunteer = false;
+  return (
+    <div className="desktop-navbar">
+      {isVolunteer && <Link to="/">isAdmin</Link>}
+      {isVolunteer && <Link to="/">isAdmin</Link>}
+      {isAdmin && <Link to="/analytics" className="desktop-navbar-link">Analytics</Link>}
+      {/* <Link to="/" className="desktop-navbar-link">Temp Link</Link> */}
+      {isAdmin && <Link to="/events" className="desktop-navbar-link">Events</Link>}
+      {isAdmin && <Link to="/inventory" className="desktop-navbar-link">Inventory</Link>}
+      {isAdmin && <Link to="/users" className="desktop-navbar-link">Users</Link>}
+    </div>
+  );
+};
 
 export default DesktopNavbar;
