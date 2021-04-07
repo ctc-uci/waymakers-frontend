8131cb47fff36f2699e14cba54517f2992d79110
Jordan Young <jordanby@uci.edu>
2021-04-07 10:26:14 -0700

    start work on mobile navbar and hamburger menu

--- src/components/header/mobile/mobileNavbar.js
+++ src/components/header/mobile/mobileNavbar.js
@@ -5,10 +5,37 @@ import HamburgerIcon from '../../../assets/hamburgerIcon.svg';
 
 const MobileNavbar = () => (
   <div className="navbar">
-    <img
+    {/* <img
       src={HamburgerIcon}
       alt="Hamburger Icon"
-    />
+      className="hamburgerButton"
+    /> */}
+    <button onClick={myFunction}>
+      <img
+        src={HamburgerIcon}
+        alt="Hamburger Icon"
+        className="hamburgerButton"
+      />
+    </button>
+    <a href="#home" className="active">Logo</a>
+    { /* Navigation links (hidden by default) */}
+    <div id="navBarLinks">
+      <a href="https://www.google.com/">Profile</a>
+      <a href="https://www.google.com/">Events</a>
+      <a href="https://www.google.com/">My Hours</a>
+      <a href="https://www.google.com/">My Hours</a>
+      <a href="https://www.google.com/">Logout</a>
+    </div>
+    {/* <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links --> */}
+    {
+  /* eslint-disable-next-line anchor-is-valid */
+}
+    <a href="javascript:void(0);" className="hamburgerButton" onClick="myFunction()">
+      <button className="hamburgerButton">
+        Button
+      </button>
+      <i className="fa fa-bars"></i>
+    </a>
   </div>
 );
 
