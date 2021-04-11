13f3d4ef5cab5c02b394a19f7cdae6932aa4bcd5
Mingjia Wang <mingjiawangdev@gmail.com>
2021-04-10 19:33:00 -0700

    Added mobile styles for availability component

--- src/components/dashboard/availability-component/volunteerAvailability/viewAvailability/viewAvailability.js
+++ src/components/dashboard/availability-component/volunteerAvailability/viewAvailability/viewAvailability.js
@@ -4,20 +4,36 @@ import ScheduleSelector from 'react-schedule-selector';
 
 import './viewAvailability.css';
 
-const ViewAvailability = ({ availabilities, startWeek }) => (
-  <div className="viewAvailCard">
-    <ScheduleSelector
-      selection={availabilities}
-      selectionScheme="square"
-      startDate={startWeek}
-      numDays={7}
-      minTime={7}
-      maxTime={19}
-      hourlyChunks={1}
-      dateFormat="ddd"
-    />
-  </div>
-);
+const ViewAvailability = ({ availabilities, startWeek }) => {
+  const renderCell = (time, selected, refSetter) => (
+    <div
+      className="grid-cell"
+      style={{ backgroundColor: `${selected ? '#799CA8' : '#F2FBFC'}` }}
+      ref={refSetter}
+    >
+      {' '}
+    </div>
+  );
+
+  return (
+    <div className="viewAvailCard">
+      <ScheduleSelector
+        selection={availabilities}
+        selectionScheme="square"
+        startDate={startWeek}
+        numDays={7}
+        minTime={7}
+        maxTime={19}
+        hourlyChunks={1}
+        selectedColor="rgba(121, 156, 168, 1)"
+        rowGap={0}
+        columnGap={0}
+        dateFormat="ddd"
+        renderDateCell={renderCell}
+      />
+    </div>
+  );
+};
 
 ViewAvailability.propTypes = {
   availabilities: PropTypes.arrayOf(PropTypes.instanceOf(Date)).isRequired,
