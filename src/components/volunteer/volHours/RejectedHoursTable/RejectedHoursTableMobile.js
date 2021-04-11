da8a78841b0a6194fccd084a4d81d61e573a5183
Harrison <harrisonhuang00@gmail.com>
2021-04-11 01:26:04 -0700

    Add resubmit popup on mobile

--- src/components/volunteer/volHours/RejectedHoursTable/RejectedHoursTableMobile.js
+++ src/components/volunteer/volHours/RejectedHoursTable/RejectedHoursTableMobile.js
@@ -1,4 +1,6 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import {
   MobileTable, MobileTableRowHeader, MobileTableRow, MobileTableContent, Divider,
@@ -8,26 +10,59 @@ import {
 } from '../../../../common/Card';
 import { formatDate, DATE_FORMAT } from '../../../../common/utils';
 
+import ResubmitHoursPopup from './ResubmitHoursPopup';
+
+const UpdateButton = styled.button`
+  border-radius: 25px;
+  background: #5D9A64;
+  color: white;
+  border: none;
+  text-decoration: none;
+  width: 65%;
+`;
+
+const Row = ({
+  eventName, rejectedNotes, startTime, endTime,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return (
+    <MobileTableRow>
+      <MobileTableRowHeader>{eventName}</MobileTableRowHeader>
+      <Divider />
+      <MobileTableContent>
+        {`Notes: ${rejectedNotes}`}
+      </MobileTableContent>
+      <MobileTableContent>
+        {`Start Date/Time: ${formatDate(startTime, DATE_FORMAT.MY_HOURS)}`}
+      </MobileTableContent>
+      <MobileTableContent>
+        {`End Date/Time: ${formatDate(endTime, DATE_FORMAT.MY_HOURS)}`}
+      </MobileTableContent>
+      <MobileTableContent>
+        <UpdateButton type="button" onClick={() => setIsModalOpen(true)}>Submit</UpdateButton>
+        {isModalOpen && (
+        <ResubmitHoursPopup
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          eventTitle={eventName}
+        />
+        )}
+      </MobileTableContent>
+    </MobileTableRow>
+  );
+};
+
 const RejectedHoursTableMobile = ({ rejectedHours }) => (
   <TitledCard title="Rejected Hours">
     <MobileTable>
       {rejectedHours && rejectedHours.map((rejectedHour) => (
-        <MobileTableRow>
-          <MobileTableRowHeader>{rejectedHour.eventName}</MobileTableRowHeader>
-          <Divider />
-          <MobileTableContent>
-            {`Location: ${rejectedHour.location}`}
-          </MobileTableContent>
-          <MobileTableContent>
-            {`Start Date/Time: ${formatDate(rejectedHour.startTime, DATE_FORMAT.MY_HOURS)}`}
-          </MobileTableContent>
-          <MobileTableContent>
-            {`End Date/Time: ${formatDate(rejectedHour.endTime, DATE_FORMAT.MY_HOURS)}`}
-          </MobileTableContent>
-          <MobileTableContent>
-            {`Hours: ${rejectedHour.hours}`}
-          </MobileTableContent>
-        </MobileTableRow>
+        <Row
+          key={rejectedHour.eventName}
+          eventName={rejectedHour.eventName}
+          rejectedNotes={rejectedHour.rejectedNotes}
+          startTime={rejectedHour.startTime}
+          endTime={rejectedHour.endTime}
+        />
       ))}
     </MobileTable>
   </TitledCard>
