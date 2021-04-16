321a5e564977028e587627dc0dbf7fd4ca5ede54
Alan Chang <alan@alanchang.xyz>
2021-04-16 02:14:48 -0700

    Start on desktop navbar menu popping up from profile picture

--- src/components/header/desktop/desktopNavbar.js
+++ src/components/header/desktop/desktopNavbar.js
@@ -1,14 +1,75 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useRef } from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import { instanceOf } from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';
+import handleOutsideClick from '../../../common/handleOutsideClick';
+import GoogleAuthService from '../../../services/firebase/firebase';
 
 import './desktopNavbar.css';
 
-const DesktopNavbar = () => {
+const DesktopNavbar = (props) => {
   // dummy isAdmin & isVolunteer vars; change with addition of logic to get user perms
   const isAdmin = true;
   const isVolunteer = false;
+
+  const { cookies } = props;
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+  const history = useHistory();
+
+  // TODOS:
+  // - finish styling the profile menu; pretty sure logout works but confirm just in case
+  // - this shouldn't display when it's on a login/register page, also needs a
+  // "switch to admin view/switch to volunteer view" option for the dashboard page -
+  // look into useLocation() in react-router-dom to achieve this conditional rendering
+  // - the "submit hours" popup will have to show the popup to submit hours for events right on
+  // the page without navigating away - look into how that was implemented on the "add hours" pg
+
+  // Close profile menu when user clicks outside of it
+  handleOutsideClick(ref, () => {
+    setOpen(false);
+  });
+
+  async function logout() {
+    try {
+      setOpen(false);
+      await GoogleAuthService.auth.signOut();
+      history.push('/login');
+      // Removing session cookie
+      cookies.remove('accessToken');
+      cookies.remove('userId');
+      // Sign-out successful
+    } catch (err) {
+      console.log('Logout failed');
+    }
+  }
+
+  const toggleMenuOpen = () => {
+    if (open) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  };
+
+  const profileMenu = (
+    <div className="navbar-profile-menu-container">
+      <a href="/profile" className="navbar-profile-menu-link">
+        <div className="navbar-profile-menu-option">View Profile</div>
+      </a>
+      <button
+        type="button"
+        label="navbar-logout-button"
+        className="navbar-profile-menu-option"
+        onClick={logout}
+      >
+        Log Out
+      </button>
+    </div>
+  );
+
   return (
-    <div className="desktop-navbar">
+    <div className="desktop-navbar" ref={ref}>
       {isAdmin && <Link to="/analytics" className="desktop-navbar-link">Analytics</Link>}
       <Link to="/events" className="desktop-navbar-link">Events</Link>
       {isVolunteer && <Link to="/volunteer/hours" className="desktop-navbar-link">My Hours</Link>}
@@ -21,9 +82,19 @@ const DesktopNavbar = () => {
       {isAdmin && <Link to="/inventory" className="desktop-navbar-link">Inventory</Link>}
       {isAdmin && <Link to="/users" className="desktop-navbar-link">Users</Link>}
       {/* <div className="desktop-navbar-pfp" /> */}
-      <div className="profile" />
+      <button
+        type="button"
+        label="profile-picture"
+        className="profile"
+        onClick={toggleMenuOpen}
+      />
+      {open && profileMenu}
     </div>
   );
 };
 
-export default DesktopNavbar;
+DesktopNavbar.propTypes = {
+  cookies: instanceOf(Cookies).isRequired,
+};
+
+export default withCookies(DesktopNavbar);
