a8612c3e16ca9ba2d493b2e846896f2e64995309
Pursain <harrisonhuang00@gmail.com>
2021-04-21 00:19:53 -0700

    Migrate to useEvent hook

--- src/components/admin/volunteer-event-aggregate-page/event-data-page/event-details/EventDetails.js
+++ src/components/admin/volunteer-event-aggregate-page/event-data-page/event-details/EventDetails.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import useEvent from './useEvent';
 
-import { WMKBackend } from '../../../../../common/utils';
-import { TitledCard } from '../../../../../common/Card';
 import clockIcon from '../../../../../assets/clock.svg';
 import locationPinIcon from '../../../../../assets/blueLocationPin.svg';
 
@@ -19,26 +18,18 @@ const monthList = [
 ];
 
 const Overview = (prop) => {
+  const [event, eventMeta] = useEvent(prop.event.id);
+  console.log(eventMeta);
+
   const suffixDict = {
     1: 'st',
     2: 'nd',
     3: 'rd',
   };
 
-  const [eventInfo, setEventInfo] = useState([]);
-
-  const getEventInfo = async () => {
-    const currentEvent = await WMKBackend.get(`/events/${prop.event.id}`);
-    setEventInfo(currentEvent.data[0]);
-  };
-
-  useEffect(() => {
-    getEventInfo();
-  }, []);
-
   const prettifyDate = () => {
-    const startTime = new Date(eventInfo.startTime);
-    const endTime = new Date(eventInfo.endTime);
+    const startTime = new Date(event.startTime);
+    const endTime = new Date(event.endTime);
 
     const differentDays = startTime.getFullYear() !== endTime.getFullYear()
                           || startTime.getMonth() !== endTime.getMonth()
@@ -56,33 +47,46 @@ const Overview = (prop) => {
   };
 
   const prettifyTime = () => {
-    const startTime = new Date(eventInfo.startTime);
-    const endTime = new Date(eventInfo.endTime);
+    const startTime = new Date(event.startTime);
+    const endTime = new Date(event.endTime);
     const startMeridiem = startTime.getHours() < 12 ? 'AM' : 'PM';
     const endMeridiem = endTime.getHours() < 12 ? 'AM' : 'PM';
 
     return `${startTime.toLocaleTimeString().slice(0, -6)}${startMeridiem} to ${endTime.toLocaleTimeString().slice(0, -6)}${endMeridiem}`;
   };
 
+  if (eventMeta.isInitializing) {
+    return <div>isInitializing</div>;
+  }
+
+  if (eventMeta.isLoading) {
+    return <div>isLoading</div>;
+  }
+
+  if (eventMeta.error) {
+    return <div>errored</div>;
+  }
+
+  if (event.length === 0) {
+    return <div>no data</div>;
+  }
+
   return (
-    <>
-      {eventInfo.length !== 0 && (
-        <TitledCard title="Event Details">
-          <div className="overview-container">
-            <p className="event-date">{prettifyDate()}</p>
-            <div className="event-info">
-              <img className="event-info-icon" src={clockIcon} alt="time" />
-              <span className="event-info-detail">{prettifyTime()}</span>
-            </div>
-            <div className="event-info">
-              <img className="event-info-icon" src={locationPinIcon} alt="location" />
-              <span className="event-info-detail">{eventInfo.location}</span>
-            </div>
-            <p className="event-description">{eventInfo.description}</p>
-          </div>
-        </TitledCard>
-      )}
-    </>
+    <div className="event-details-aggregate">
+      <h2 className="event-details-title">Event Details</h2>
+      <div className="overview-container">
+        <p className="event-date">{prettifyDate()}</p>
+        <div className="event-info">
+          <img className="event-info-icon" src={clockIcon} alt="time" />
+          <span className="event-info-detail">{prettifyTime()}</span>
+        </div>
+        <div className="event-info">
+          <img className="event-info-icon" src={locationPinIcon} alt="location" />
+          <span className="event-info-detail">{event.location}</span>
+        </div>
+        <p className="event-description">{event.description}</p>
+      </div>
+    </div>
   );
 };
 
