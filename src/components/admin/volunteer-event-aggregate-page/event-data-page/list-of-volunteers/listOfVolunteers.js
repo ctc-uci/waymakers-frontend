ff5738ed1528b1028aa15f5663b7a61a5e999fc7
Harrison <harrisonhuang00@gmail.com>
2021-04-30 14:05:49 -0700

    Add initial pagination impl

--- src/components/admin/volunteer-event-aggregate-page/event-data-page/list-of-volunteers/listOfVolunteers.js
+++ src/components/admin/volunteer-event-aggregate-page/event-data-page/list-of-volunteers/listOfVolunteers.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import VolunteerTable from './volunteerTable';
 import VolunteerTableMobile from './volunteerTableMobile';
 import useMobileWidth from '../../../../../common/useMobileWidth';
@@ -8,62 +8,111 @@ import { WMKBackend } from '../../../../../common/utils';
 
 import './listOfVolunteers.css';
 
+const ROWS_PER_PAGINATION = 6;
+
+const SORT_BY_OPTIONS = [
+  {
+    displayName: 'Last Name A-Z',
+    compareFunc: (a, b) => (a.lastname > b.lastname ? 1 : -1),
+  },
+  {
+    displayName: 'Last Name Z-A',
+    compareFunc: (a, b) => (a.lastname < b.lastname ? 1 : -1),
+  },
+  {
+    displayName: 'Most Hours',
+    compareFunc: (a, b) => (a.sum < b.sum ? 1 : -1),
+  },
+  {
+    displayName: 'Least Hours',
+    compareFunc: (a, b) => (a.sum > b.sum ? 1 : -1),
+  },
+];
+
 const ListOfVolunteers = (prop) => {
   const isMobile = useMobileWidth();
   const [allVolunteers, setAllVolunteers] = useState([]);
-
-  const [sortingMethod, setSortingMethod] = useState('0');
-
   const [totalHours, setTotalHours] = useState(0);
+  const [sortByOption, setSortByOption] = useState(0);
+  const [paginatedIndex, setPaginatedIndex] = useState(0);
+  const [totalNumberOfPages, setTotalNumberOfPages] = useState(0);
+
+  const paginatedVolunteers = useMemo(() => {
+    if (!allVolunteers) { return []; }
+    return allVolunteers
+      .sort(SORT_BY_OPTIONS[sortByOption].compareFunc)
+      .slice(paginatedIndex * ROWS_PER_PAGINATION, (paginatedIndex + 1) * ROWS_PER_PAGINATION);
+  }, [allVolunteers, paginatedIndex, sortByOption]);
 
   const paramQuery = {
     params: {
       event: prop.event.id,
-      sortingMethod,
     },
   };
 
   const getAllVolunteers = async () => {
+    // const TEST_DATA = [...Array(103)].map((a, i) => ({
+    //   birthdate: '2021-01-24T08:00:00.000Z',
+    //   date_part: 0,
+    //   division: null,
+    //   email: 'admin@uci.edu',
+    //   firstname: `AdminFirst${i}`,
+    //   gender: 'Prefer Not To Say',
+    //   lastname: `AdminLast${i}`,
+    //   location_street_2: null,
+    //   locationcity: 'Irvine',
+    //   locationstate: 'CA',
+    //   locationstreet: '1 Test Street',
+    //   locationzip: 92697,
+    //   permissions: 'Admin',
+    //   phone: '5101234567',
+    //   profile_picture: 'https://wmk-dev-1.s3.us-west-1.amazonaws.com/3K1HBRGdf2cMKvROFIhBF322gZy21',
+    //   sum: i,
+    //   tier: 1,
+    //   userid: '3K1HBRGdf2cMKvROFIhBF322gZy2',
+    //   verified: true,
+    // }));
+    // setAllVolunteers(TEST_DATA);
+    // setTotalNumberOfPages(Math.ceil(TEST_DATA.length / ROWS_PER_PAGINATION));
+    // setTotalHours(TEST_DATA
+    //   .map((v) => v.sum)
+    //   .reduce((acc, v) => acc + v, 0));
+
     const volunteers = await WMKBackend.get('/volunteerData/all/', paramQuery);
-    // TODO: remove this temporary overflow test
-    setAllVolunteers([...volunteers.data,
-      ...volunteers.data, ...volunteers.data, ...volunteers.data,
-      ...volunteers.data, ...volunteers.data, ...volunteers.data,
-      ...volunteers.data, ...volunteers.data, ...volunteers.data,
-      ...volunteers.data, ...volunteers.data, ...volunteers.data,
-      ...volunteers.data, ...volunteers.data, ...volunteers.data,
-      ...volunteers.data, ...volunteers.data, ...volunteers.data]);
-    console.log(volunteers.data);
-    if (volunteers.data.length) {
-      setTotalHours(
-        volunteers.data.map((v) => v.sum).reduce((acc, v) => acc + v),
-      );
-    }
+    // console.log(volunteers.data);
+    setAllVolunteers(volunteers.data);
+    setTotalNumberOfPages(Math.ceil(volunteers.data.length / ROWS_PER_PAGINATION));
+    setTotalHours(volunteers.data
+      .map((v) => v.sum)
+      .reduce((acc, v) => acc + v, 0));
   };
 
   useEffect(() => {
-    // TODO: havent confirmed if sorting works
     getAllVolunteers();
-  }, [sortingMethod]);
+  }, []);
 
   return (
     // <div className="list-of-volunteer">
     <TitledCard title="List of Volunteers" className="list-of-volunteer-container" cardClassName="list-of-volunteer">
       <div className="sort-by-dropdown">
         Sort by:
-        <select className="sort-by" value={sortingMethod} onChange={(e) => setSortingMethod(e.target.value)}>
-          <option className="sort-by-items" value="0">A-Z</option>
-          <option className="sort-by-items" value="1">Z-A</option>
-          <option className="sort-by-items" value="2">Most Hours</option>
-          <option className="sort-by-items" value="3">Least Hours</option>
+        <select className="sort-by" value={sortByOption} onChange={(e) => setSortByOption(e.target.value)}>
+          {SORT_BY_OPTIONS.map((sortOption, i) => <option className="sort-by-items" value={i}>{sortOption.displayName}</option>)}
         </select>
       </div>
       {isMobile
         ? (
-          <VolunteerTableMobile data={allVolunteers} />
+          <VolunteerTableMobile data={paginatedVolunteers} />
         ) : (
-          <VolunteerTable data={allVolunteers} />
+          <VolunteerTable data={paginatedVolunteers} />
         )}
+      <div className="pagination-indicator">
+        <button type="button" onClick={() => { setPaginatedIndex((old) => Math.max(0, old - 1)); }}>{'<'}</button>
+        <div>{paginatedIndex + 1}</div>
+        <div>/</div>
+        <div>{totalNumberOfPages}</div>
+        <button type="button" onClick={() => { setPaginatedIndex((old) => Math.min(totalNumberOfPages - 1, old + 1)); }}>{'>'}</button>
+      </div>
       <div className="total-stats">
         <p>
           Total People:
